e4d8b96bf28be0e02dec37e9f368b544d1ad8b05
qaptoR <rocco.ruscitti@outlook.com>
2024-07-07 13:52:48 -0700

    Blast doors, enemy spawner, and scene manager

---
+++ scenes/levels/settings_menu/settings_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_close_button_pressed () -> void:
+    SceneManager.hide_settings_menu()
---
+++ scenes/objects/blast_doors/movement.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+const SCROLL_SPEED :float = 100
+
+
+func _process (_delta_ :float) -> void:
+    if owner.global_position.x <= -100: owner.queue_free()
+    var velocity :float = SCROLL_SPEED * _delta_
+    owner.global_position.x -= velocity
