f54001e4ff66e3ed37cf97c6f6f297907abd522b
JoshEthan <joshuaeris@gmail.com>
2024-07-10 20:25:58 -0600

    Boss Mechanics

--- scenes/auto/scene_manager/scene_manager.gd
+++ scenes/auto/scene_manager/scene_manager.gd
@@ -10,40 +10,40 @@ var settings_menu_instance: Node = null
 
 
 func load_main_menu():
-    get_tree().change_scene_to_packed(main_menu_scene)
+	get_tree().change_scene_to_packed(main_menu_scene)
 
 
 func load_level(level_index: int):
-    if 0 <= level_index and level_index < level_scenes.size():
-        get_tree().change_scene_to_packed(level_scenes[level_index])
-    else:
-        print("Invalid level index")
+	if 0 <= level_index and level_index < level_scenes.size():
+		get_tree().change_scene_to_packed(level_scenes[level_index])
+	else:
+		print("Invalid level index")
 
 
 func load_settings_menu():
-    if settings_menu_instance == null:
-        settings_menu_instance = settings_menu_scene.instantiate()
-        get_tree().root.add_child(settings_menu_instance)
-    else:
-        get_tree().root.remove_child(settings_menu_instance)
-        get_tree().root.add_child(settings_menu_instance)
+	if settings_menu_instance == null:
+		settings_menu_instance = settings_menu_scene.instantiate()
+		get_tree().root.add_child(settings_menu_instance)
+	else:
+		get_tree().root.remove_child(settings_menu_instance)
+		get_tree().root.add_child(settings_menu_instance)
 
 
 func show_settings_menu():
-    load_settings_menu()
-    get_tree().paused = true
+	load_settings_menu()
+	get_tree().paused = true
 
 
 func hide_settings_menu():
-    if settings_menu_instance != null:
-        settings_menu_instance.hide()
-    get_tree().paused = false
-    var current_scene = get_tree().current_scene
-    # 
+	if settings_menu_instance != null:
+		settings_menu_instance.hide()
+	get_tree().paused = false
+	var current_scene = get_tree().current_scene
+	# 
 
 func change_scene(scene_ :PackedScene) -> void:
-    if current_level_instance != null:
-        current_level_instance.queue_free()
-        current_level_instance = null
-    var new_scene_instance = scene_.instance()
-    get_tree().root.add_child(new_scene_instance)
+	if current_level_instance != null:
+		current_level_instance.queue_free()
+		current_level_instance = null
+	var new_scene_instance = scene_.instance()
+	get_tree().root.add_child(new_scene_instance)
---
+++ scenes/levels/1_main/main.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var scene1 = preload("res://scenes/levels/test_base_level/base_level.tscn")
+var scene2 = preload("res://scenes/levels/boss_room/boss_room.tscn")
