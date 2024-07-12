ba85cb87c6c61f52dd026189e69b72dff219cf8d
qaptoR <rocco.ruscitti@outlook.com>
2024-07-12 04:30:06 -0700

    we need to standardize using spaces over tabs

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 func _ready () -> void:
-	get_node('process/Health').update.connect(get_node('view/Health').handle_update)
+    get_node('process/Health').update.connect(get_node('view/Health').handle_update)
 
-	var tween = get_tree().create_tween().set_loops()
-	tween.tween_interval(randf_range(2.0, 4.0)).finished.connect(func():
-		get_node('process/Health').take_damage(floor(randf_range(5, 15)))
-	)
+    # var tween = get_tree().create_tween().set_loops()
+    # tween.tween_interval(randf_range(2.0, 4.0)).finished.connect(func():
+    #     get_node('process/Health').take_damage(floor(randf_range(5, 15)))
+    # )
