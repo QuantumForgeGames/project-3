a2144bdbd09eb8aeba9dfb954f387b0ce95e6bf1
JoshEthan <joshuaeris@gmail.com>
2024-07-12 18:06:25 -0600

    Updated images. Added fuel to player jetpack.

--- scenes/entities/player/scripts/movement.gd
+++ scenes/entities/player/scripts/movement.gd
@@ -8,20 +8,45 @@ const MOVE_SPEED     :float = 200.0
 const MAX_FALL_SPEED :float = 500.0
 
 var _is_jetpacking = false
+var fuel = 100
+@onready var label = $"../../Label"
 
 
 func _process (_delta_ :float) -> void:
+	label.text = str(fuel)
 
 	var direction :float = 0.
-	if Input.is_action_pressed("move_left"): direction -= 1
-	if Input.is_action_pressed("move_right"): direction += 1
-	if Input.is_action_pressed("move_up"): _is_jetpacking = true
-	else: _is_jetpacking = false
+	if Input.is_action_pressed("move_left") and fuel > 0: 
+		direction -= 1
+		if fuel > 0:
+			fuel -= 0.5
+		
+	if Input.is_action_pressed("move_right") and fuel > 0: 
+		direction += 1
+		if fuel > 0:
+			fuel -= 0.5
+		
+	if Input.is_action_pressed("move_up") and fuel > 0:
+		_is_jetpacking = true
+	else: 
+		_is_jetpacking = false
 
 	owner.velocity.x = direction * MOVE_SPEED
 
-	if not _is_jetpacking: owner.velocity.y += GRAVITY
-	else:                  owner.velocity.y = JETPACK_FORCE
+	if not _is_jetpacking: 
+		owner.velocity.y += GRAVITY
+	else:                  
+		owner.velocity.y = JETPACK_FORCE
+		if fuel > 0:
+			fuel -= 0.5
+		elif fuel < 0:
+			fuel = 0
+			
+	if owner.is_on_floor():
+		if fuel < 100:
+			fuel += 2
+		elif fuel > 100:
+			fuel = 100
 
 	if owner.velocity.y > MAX_FALL_SPEED:
 		owner.velocity.y = MAX_FALL_SPEED
