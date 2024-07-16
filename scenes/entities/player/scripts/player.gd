ee871344312a288eac3cb57e4f93d52952651c37
qaptoR <rocco.ruscitti@outlook.com>
2024-07-16 13:20:23 -0700

    completed player health carry through scene loading

--- scenes/entities/player/scripts/player.gd
+++ scenes/entities/player/scripts/player.gd
@@ -1,12 +1,10 @@
+
+
+class_name Player
 extends CharacterBody2D
 
 
 func _ready () -> void:
     get_node('process/Health').update.connect(get_node('view/Health').handle_update)
     get_node('process/Shooting').update.connect(get_node('view/Laser').handle_update)
-    get_node('process/Shooting').overheat.connect(get_node('view/Overheat').handle_update)
-
-    # var tween = get_tree().create_tween().set_loops()
-    # tween.tween_interval(randf_range(2.0, 4.0)).finished.connect(func():
-    #     get_node('process/Health').take_damage(floor(randf_range(5, 15)))
-    # )
+    # get_node('process/Shooting').overheat.connect(get_node('view/Overheat').handle_update)
--- scripts/systems/basic_health.gd
+++ scripts/systems/basic_health.gd
@@ -11,24 +11,46 @@ const MAX_HEALTH = 100.0
 @export var _health :float = 100.0
 @export var _owner :Node = null
 
+var id :String = ''
+
 
 func _ready():
-	_health = MAX_HEALTH
-	update.emit(_health)
+    GameManager.sync_data.connect(on_sync_update)
+    id = '%s/%s' %[owner.name, owner.get_path_to(self)]
+    var is_ready = GameManager.get_property(id, 'is_ready')
+    if not is_ready:
+        prints('readyup', owner.name, id)
+        _health = MAX_HEALTH
+        GameManager.set_property(id, 'is_ready', true)
+    # else:
+        # GameManager.sync_data.emit(false)
+    update.emit(_health)
 
 
 func take_damage (damage_ :float) -> void:
-	_health -= damage_
-	if _health <= 0.0:
-		died.emit()
-		_owner.queue_free()
-		return
-	update.emit(_health)
+    _health -= damage_
+    if _health <= 0.0:
+        died.emit()
+        _owner.queue_free()
+        return
+    update.emit(_health)
 
 
 func take_heal (heal_ :float) -> void:
-	_health += heal_
-	if _health > MAX_HEALTH:
-		_health = MAX_HEALTH
-	update.emit(_health)
+    _health += heal_
+    if _health > MAX_HEALTH:
+        _health = MAX_HEALTH
+    update.emit(_health)
 
+
+func on_sync_update (upstream_ :bool) -> void:
+
+    prints('syncin', self, owner.name, id)
+    if upstream_:
+        GameManager.set_property(id, 'health', _health)
+    else:
+        var value = GameManager.get_property(id, 'health')
+        if not value: return
+        _health = value
+        prints(owner.name, 'health sync', _health)
+        update.emit(_health)
